9c96d022e9610ebfed554e5484e416c5335e0b0d
Park Junghwan <parkjh9010@gmail.com>
2024-02-23 15:31:54 +0900

    add scenario manager

--- Cog.Framework/Device/PLC/PlcAddressMap.cs
+++ Cog.Framework/Device/PLC/PlcAddressMap.cs
@@ -10,8 +10,43 @@ namespace Cog.Framework.Device.PLC
     {
     }
 
-    public enum AddressMap
+    public enum BaseAddressMap
     {
-        Heartbeat = 200,
+#if !SDBIO_VENT
+        PLC_BaseAddress = 27000,
+        PC_BaseAddress = 27000,
+#endif
+
+#if SDBIO_PATH
+        PLC_BaseAddress = 28000,
+        PC_BaseAddress = 28000,
+#endif
+    }
+
+    public enum PlcCommonMap
+    {
+        PC_Model_No = 0,
+        PLC_Model_No = 100,
+
+        Vision_Ready = 4,
+
+        PC_Status = 6,
+        PLC_Command = 106,
+        Alive = 200,
+
+        PLC_Time = 300,
+    }
+
+    //public enum PlcCommonCommand
+    //{
+    //    Time_Change = 6000,
+    //    Model_Change = 8000,
+    //}
+
+    public enum PlcCommand
+    {
+        StartInspection = 1100,
+        Time_Change = 6000,
+        Model_Change = 8000,
     }
 }
